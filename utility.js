2cffdbfb681ec25ac738eb0a0cbabb62390aa569
k_lychovski <k.lychovski@caspiantechnologies.org>
2022-05-23 17:25:00 +0300

    utility for getting the current date and time

---
+++ utility.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
+console.log('argv', argv)
+
+if (argv) {
+  console.log(`Current date and time in ISO format: ${new Date().toISOString()}`)
+}
+if (argv.month | argv.m) {
+  console.log(`Current month: ${new Date().getMonth()}`)
+}
+if (argv.year) {
+  console.log(`Current year: ${new Date().getFullYear()}`)
+}
+if (argv.date | argv.d) {
+  console.log(`Current date: ${new Date().getDate()}`)
+}
+
