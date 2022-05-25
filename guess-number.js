0092c14efc0d1e1aa529c2504f3adc0fc8aa3f81
Konstantin <kos8533@yandex.ru>
2022-05-25 18:28:16 +0300

    full task 2

--- guess-number.js
+++ guess-number.js
@@ -1,32 +1,29 @@
 #!/usr/bin/env node
 
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
-
-const argv = yargs(hideBin(process.argv)).argv;
-
+const readline = require('readline');
+const inputConsole = readline.createInterface(process.stdin);
 let start = 0;
 let end = 0;
-let hiddenNumber = 0;
+let hiddenNumber;
 
 const generatorRandom = () => {
-
-    start = Math.floor(Math.random() * 10) + 1 // (1 до 10]
-    end = Math.floor(Math.random() * 10) + 11 // (10 до 20]
-    hiddenNumber = Math.floor(Math.random() * (end - start + 1)) + start
-
-    // console.log('start', start)
-    // console.log('end', end)
-    console.log('hiddenNumber', hiddenNumber)
-
-    console.log(`Угадай число в диапазоне от ${start} до ${end}`)
-    return start, end, hiddenNumber
+    start = Math.floor(Math.random() * 10) + 1; // (1 до 10]
+    end = Math.floor(Math.random() * 10) + 11; // (10 до 20]
+    hiddenNumber = Math.floor(Math.random() * (end - start + 1)) + start;
+    console.log(`Угадай число в диапазоне от ${start} до ${end}`);
+    return start, end, hiddenNumber;
 }
 
-if (argv.luckyNumber === hiddenNumber) {
-    console.log("Поздравляю, Вы угадали число")
-} else {
-    console.log("Сожалею, Вы не угадали число")
-}
+generatorRandom();
 
-module.exports = generatorRandom()
+inputConsole.on('line', (entered_number) => {
+    if (+entered_number === hiddenNumber) {
+        console.log("Поздравляю, Вы угадали число!!!")
+        inputConsole.close();
+        return;
+    } else {
+        console.log("Сожалею, Вы не угадали число")
+        console.log('entered_number', entered_number)
+        generatorRandom();
+    }
+});
